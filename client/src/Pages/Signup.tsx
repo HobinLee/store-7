7f1ef411fa3a23258ba35f6fb3fe92561c00239e
leejeongmin <danmin20@khu.ac.kr>
2021-08-09 23:30:22 +0900

    feat: login/signup

---
+++ client/src/Pages/Signup.tsx
@@ -0,0 +1,11 @@
+import { PageWrapper } from "@/shared/styled";
+import React from "react";
+import styled from "styled-components";
+
+const SignupPage = () => {
+  return <Wrapper></Wrapper>;
+};
+
+const Wrapper = styled(PageWrapper)``;
+
+export default SignupPage;
