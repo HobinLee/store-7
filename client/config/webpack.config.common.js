285ecbd2434849b259b2ca0da94b3645ccff4a19
Lee Jeong Min <danmin20@khu.ac.kr>
2021-08-09 17:13:19 +0900

    chore: client setup

---
+++ client/config/webpack.config.common.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = (env) => {
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.join(__dirname, '../dist'),
+      filename: 'bundle.js',
+      publicPath: '/',
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      alias: {
+        '@': path.resolve(__dirname, '../src/'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.(png|jpe?g|gif|woff|woff2|ttf|svg|ico)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new dotenv({
+        path: env.production ? './env/.env' : './env/dev.env',
+      }),
+    ],
+  };
+};
