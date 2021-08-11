42fdb9b0d2fef9f365b61864d9810d17cc9dd824
jjunyjjuny <jjuny546@gmail.com>
2021-08-11 20:48:11 +0900

    feat: table 스타일, options 컴포넌트

---
+++ client/src/Pages/MyPage/tableTds.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProductOption = ({ url, title }) => {
+  return (
+    <ProductOptionWrapper>
+      <div className="thumbnail">
+        <img src={url} />
+      </div>
+      <h4 className="title">{title}</h4>
+    </ProductOptionWrapper>
+  );
+};
+
+const ProductOptionWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  .thumbnail {
+    width: 4rem;
+    height: 5rem;
+    img {
+      display: block;
+      width: 100%;
+    }
+    margin-right: 1rem;
+  }
+
+  .title {
+    ${({ theme }) => theme.font.small}
+    font-weight: bold;
+  }
+`;
+
+const PriceAndAmount = () => {
+  return <PriceAndAmountWrapper></PriceAndAmountWrapper>;
+};
+
+const PriceAndAmountWrapper = styled.div``;
+
+const Sum = () => {
+  <SumWrapper></SumWrapper>;
+};
+
+const SumWrapper = styled.div``;
+
+const TableForm = {
+  ProductOption,
+  PriceAndAmount,
+  Sum,
+};
+
+export default TableForm;
