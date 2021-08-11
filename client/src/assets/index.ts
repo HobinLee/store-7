18e9a040e220e5a124a7fc5a6614c7f3e00bd451
jjunyjjuny <jjuny546@gmail.com>
2021-08-12 01:19:12 +0900

    chore: brush  svg 추가

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -2,6 +2,12 @@ import hamburger from "./hamburger.svg";
 import banner1 from "./banner1.gif";
 import banner2 from "./banner2.gif";
 import banner3 from "./banner3.gif";
+import Bursh1 from "./brush1.svg";
+import Brush2 from "./brush2.svg";
+import Brush3 from "./brush3.svg";
+import Brush4 from "./brush4.svg";
+import Brush5 from "./brush5.svg";
+import Brush6 from "./brush6.svg";
 import logo from "./logo.png";
 import MenuIcon from "./menu.svg";
 import Close from "./close.svg";
@@ -12,6 +18,12 @@ export {
   banner1,
   banner2,
   banner3,
+  Bursh1,
+  Brush2,
+  Brush3,
+  Brush4,
+  Brush5,
+  Brush6,
   logo,
   MenuIcon,
   Close,
