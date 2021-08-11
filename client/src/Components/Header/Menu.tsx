1288136ca30f7c116eba970d734d494512e6b9ee
hobinLee <viny5120@gmail.com>
2021-08-11 11:19:49 +0900

    feat: 헤더 작업 진행중

--- client/src/Components/Header/Menu.tsx
+++ client/src/Components/Header/Menu.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { ETLink } from "@/Router";
+import { MenuIcon } from "@/assets/";
 import { DropdownWrapper, DropdownItem } from "@/shared/styled";
 
 const Menu = () => {
@@ -17,15 +18,12 @@ const Menu = () => {
       onMouseEnter={() => handleMenuOpen(true)}
       onMouseLeave={() => handleMenuOpen(false)}
     >
-      <img
-        height="100"
-        src="https://mblogthumb-phinf.pstatic.net/MjAxOTA1MTdfMjg5/MDAxNTU4MDU5MjY3NzI0.La9iCTKSS9Cue6MbMeNSJADSkjSr0VMPlAsIdQYGjoYg.q_VK0tw6okzVQOBJbXGKFFGJkLJUqLVT26CZ9qe29Xcg.PNG.smartbaedal/%ED%97%A4%ED%97%A4%EB%B0%B0%EB%8B%AC%EC%9D%B4_%EC%9E%90%EB%A5%B8%EA%B2%83.png?type=w800"
-      />
+      <MenuIcon />
       {isMenuOpened && (
         <DropdownWrapper style={{ left: 0, top: "10rem" }}>
           {[0, 0, 0, 0, 0, 0, 0].map((i, idx) => (
             <ETLink to={`/category/${idx}`}>
-              <DropdownItem key={i.toString()}>asdf</DropdownItem>
+              <DropdownItem key={idx}>asdf</DropdownItem>
             </ETLink>
           ))}
         </DropdownWrapper>
@@ -39,6 +37,11 @@ const Wrapper = styled.div`
   position: relative;
   width: 10rem;
   height: 10rem;
+
+  img {
+    width: 10rem;
+    height: 10rem;
+  }
 `;
 
 export default Menu;
