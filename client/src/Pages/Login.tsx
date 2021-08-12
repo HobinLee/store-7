34b4ccff44968a1011f36480d2865d3fa9041e46
hobinLee <viny5120@gmail.com>
2021-08-12 10:00:47 +0900

    chore: 로그인 프리티어 포맷적용

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -16,47 +16,72 @@ const LoginPage = () => {
     //TODO: 로그인 요청
     setTimeout(() => {
       //로그인 처리
-      window.location.href='/';
+      window.location.href = "/";
     }, 1000);
-  }
+  };
 
   const checkSignupable = (): boolean => {
     //TODO: validion Check하기(?)
     return id.value.length > 0 && pw.value.length > 0;
-  }
-  
+  };
+
   const checkLookupable = (): boolean => {
     //TODO: validion Check하기(?)
     return name.value.length > 0 && number.value.length > 0;
-  }
+  };
 
-  return <LoginPageWrapper>
-    <LoginContent>
-      <Form onSubmit={handleSignin}>
-        <Title>회원 로그인</Title>
-        <Input placeholder="아이디 입력" value={id.value} onChange={id.onChange}/>
-        <Input placeholder="비밀번호 입력" value={pw.value} type='password' onChange={pw.onChange}/>
-        <div>아이디 저장</div>
-        <Button primary disabled={!checkSignupable()}>로그인</Button>
-        <Button>Github 로그인</Button>
-        <div className="login-form__footer">
-          <ETLink to='/signup'>회원가입</ETLink>
-          <ETLink to='/findid'>아이디 찾기</ETLink>
-          <ETLink to='/findpw'>비밀번호 찾기</ETLink>
-        </div>
-      </Form>
-      <Form>
-        <Title>비회원 주문조회 하기</Title>
-        <Input placeholder="주문자명" value={name.value} onChange={name.onChange} />
-        <Input placeholder="주문번호" value={number.value} onChange={number.onChange} />
-        <Button disabled={!checkLookupable()}>조회하기</Button>
-        <span>주문번호와 비밀번호를 잊으신 경우, 고객센터로 문의하여 주시기 바랍니다.</span>
-      </Form>
-    </LoginContent>
-  </LoginPageWrapper>;
+  return (
+    <LoginPageWrapper>
+      <LoginContent>
+        <Form onSubmit={handleSignin}>
+          <Title>회원 로그인</Title>
+          <Input
+            placeholder="아이디 입력"
+            value={id.value}
+            onChange={id.onChange}
+          />
+          <Input
+            placeholder="비밀번호 입력"
+            value={pw.value}
+            type="password"
+            onChange={pw.onChange}
+          />
+          <div>아이디 저장</div>
+          <Button primary disabled={!checkSignupable()}>
+            로그인
+          </Button>
+          <Button>Github 로그인</Button>
+          <div className="login-form__footer">
+            <ETLink to="/signup">회원가입</ETLink>
+            <ETLink to="/findid">아이디 찾기</ETLink>
+            <ETLink to="/findpw">비밀번호 찾기</ETLink>
+          </div>
+        </Form>
+        <Form>
+          <Title>비회원 주문조회 하기</Title>
+          <Input
+            placeholder="주문자명"
+            value={name.value}
+            onChange={name.onChange}
+          />
+          <Input
+            placeholder="주문번호"
+            value={number.value}
+            onChange={number.onChange}
+          />
+          <Button disabled={!checkLookupable()}>조회하기</Button>
+          <span>
+            주문번호와 비밀번호를 잊으신 경우, 고객센터로 문의하여 주시기
+            바랍니다.
+          </span>
+        </Form>
+      </LoginContent>
+    </LoginPageWrapper>
+  );
 };
 
 const LoginPageWrapper = styled(PageWrapper)`
+  box-sizing: border-box;
   margin: auto;
   padding: 10rem;
 
@@ -88,7 +113,7 @@ const LoginPageWrapper = styled(PageWrapper)`
       width: 100%;
       border-right: 1px solid ${({ theme }) => theme.color.light_grey2};
     }
-    
+
     a:last-child {
       border: none;
     }
@@ -104,7 +129,7 @@ const LoginContent = styled.div`
 const Title = styled.h3`
   text-align: center;
   ${({ theme }) => theme.font.large}
-`
+`;
 
 const Form = styled.form`
   width: 100%;
@@ -112,6 +137,6 @@ const Form = styled.form`
   display: flex;
   flex-direction: column;
   gap: 2rem;
-`
+`;
 
 export default LoginPage;
