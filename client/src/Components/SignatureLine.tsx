e25ffd3dda6e541a30c1d7ede439573a9a2ee0b0
jjunyjjuny <jjuny546@gmail.com>
2021-08-12 23:35:31 +0900

    chore: @/assets/ 에서 / 지움

--- client/src/Components/SignatureLine.tsx
+++ client/src/Components/SignatureLine.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
-import { LINE_LINK } from "@/assets/";
+import { LINE_LINK } from "@/assets";
 
 const SignatureLine = ({
   type,
