cd43492fd8c8cf664294beb77a7ed1f7cd27ad9c
jjunyjjuny <jjuny546@gmail.com>
2021-08-12 18:14:21 +0900

    chore: 글로벌 스타일 포매팅, 더미 추가

--- client/src/styles/global-style.ts
+++ client/src/styles/global-style.ts
@@ -3,74 +3,71 @@ import { reset } from "styled-reset";
 import { media } from "./theme";
 
 export const GlobalStyle = createGlobalStyle`
-    ${reset}
-    
-    html{
-      font-size: 10px;
-      font-family: Noto Sans KR, Apple SD Gothic Neo, sans-serif;
-      font-display: fallback;
-      overflow-x: hidden;
-      color: ${({ theme }) => theme.color.title_active};
-      a{
-          text-decoration: none;
-          color: ${({ theme }) => theme.color.title_active};
-      }
-      button,
-      input,
-      optgroup,
-      select,
-      textarea {
-          color: inherit;
-          font: inherit;
-          margin: 0;
-          background: none;
-          border: none;
-      }
-      
-      input,
-      button,
-      select,
-      textarea {
-          &:focus {
-              outline: none;
-          }
-      }
+  ${reset}
 
-      textarea {
-          border: none;
-          resize: none;
-      }
-      input[type="file"] {
-          position: absolute;
-          width: 1px;
-          height: 1px;
-          padding: 0;
-          margin: -1px;
-          overflow: hidden;
-          clip:rect(0,0,0,0);
-          border: 0;
+  html {
+    font-size: 10px;
+    font-family: Noto Sans KR, Apple SD Gothic Neo, sans-serif;
+    font-display: fallback;
+    overflow-x: hidden;
+    color: ${({ theme }) => theme.color.title_active};
+    a {
+      text-decoration: none;
+      color: ${({ theme }) => theme.color.title_active};
+    }
+    button,
+    input,
+    optgroup,
+    select,
+    textarea {
+      color: inherit;
+      font: inherit;
+      margin: 0;
+      background: none;
+      border: none;
+    }
+
+    input,
+    button,
+    select,
+    textarea {
+      &:focus {
+        outline: none;
       }
     }
-    
-    .no-scroll-bar::-webkit-scrollbar {
+
+    textarea {
+      border: none;
+      resize: none;
+    }
+    input[type="file"] {
+      position: absolute;
+      width: 1px;
+      height: 1px;
+      padding: 0;
+      margin: -1px;
+      overflow: hidden;
+      clip: rect(0, 0, 0, 0);
+      border: 0;
+    }
+  }
+
+  .no-scroll-bar::-webkit-scrollbar {
+    display: none;
+  }
+  .no-scroll-bar {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+  .mobile__only {
+    display: none;
+    ${media.mobile} {
+      display: block;
+    }
+  }
+  .mobile__none {
+    ${media.mobile} {
       display: none;
     }
-    .no-scroll-bar {
-      -ms-overflow-style: none;
-      scrollbar-width: none;
-    }
-    .mobile__only {
-      display: none;
-      ${media.mobile} {
-          display: block;
-      }
-    }
-    .mobile__none {
-      ${media.mobile} {
-          display: none;
-      }
-    }
-  button {
-    cursor: pointer;
   }
 `;
