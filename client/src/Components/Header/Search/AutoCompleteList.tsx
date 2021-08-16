3c46ee663afd0ae3bfc7f510e8662eb94659fc74
hobinLee <viny5120@gmail.com>
2021-08-17 02:18:20 +0900

    feat: 자동완성 스크롤 변경

--- client/src/Components/Header/Search/AutoCompleteList.tsx
+++ client/src/Components/Header/Search/AutoCompleteList.tsx
@@ -2,9 +2,15 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import styled from "styled-components";
 
+// TODO: 그냥 테스트용 데이터고 화요일에 바로 Elastic Search 적용해볼 거에요..!
 const RandomList = [
   "물품1",
   "물품2",
+  "물품3",
+  "물품4",
+  "물품5",
+  "물품6",
+  "물품7",
   "책",
   "매거진",
   "펜",
@@ -34,14 +40,22 @@ const AutoList = ({ keyword, handleSearch }) => {
       {value}
     </li>
   ));
-  return <AutoListWrapper>{generateAutoList}</AutoListWrapper>;
+  return (
+    <AutoListWrapper>
+      {autoList.length ? (
+        generateAutoList
+      ) : (
+        <div className="no-list">관련 상품이 없습니다</div>
+      )}
+    </AutoListWrapper>
+  );
 };
 
 const AutoListWrapper = styled.ul`
   display: flex;
   flex-direction: column;
-  margin-top: 1rem;
-
+  max-height: 16rem;
+  overflow-y: scroll;
   li {
     color: ${({ theme }) => theme.color.grey1};
     padding: 1rem;
@@ -53,7 +67,7 @@ const AutoListWrapper = styled.ul`
   }
 
   .no-list {
-    color: ${({ theme }) => theme.color.grey1};
+    color: ${({ theme }) => theme.color.grey2};
   }
 `;
 
