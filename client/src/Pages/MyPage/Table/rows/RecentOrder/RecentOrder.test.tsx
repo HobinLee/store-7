d7809856221c3c6431ce96d7de99f468909b58a8
jjunyjjuny <jjuny546@gmail.com>
2021-08-19 01:32:12 +0900

    feat: test recent order

--- client/src/Pages/MyPage/Table/rows/RecentOrder/RecentOrder.test.tsx
+++ client/src/Pages/MyPage/Table/rows/RecentOrder/RecentOrder.test.tsx
@@ -2,6 +2,8 @@ import "jest-styled-components";
 import { render } from "@/utils/test-util";
 import { screen } from "@testing-library/react";
 import RecentOrder, { RecentOrderProps } from "./index";
+import { YYYYMMDD } from "@/utils/util";
+import { expectText } from "@/utils/test-util";
 
 const RecentOrderProps: RecentOrderProps = {
   id: 1,
@@ -16,9 +18,19 @@ const RecentOrderProps: RecentOrderProps = {
 };
 
 describe("<RecentOrder />", () => {
-  it("should render component in document", () => {
+  let page;
+  beforeEach(() => {
     const { container } = render(<RecentOrder {...RecentOrderProps} />);
+    page = container;
+  });
+  it("should render component in document", () => {
+    expect(page).toBeInTheDocument();
+  });
 
-    expect(container).toBeInTheDocument();
+  it("should render text in document", () => {
+    expectText(YYYYMMDD(RecentOrderProps.date));
+
+    expectText(RecentOrderProps.name);
+    expectText(RecentOrderProps.number);
   });
 });
