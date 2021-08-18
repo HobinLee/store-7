1aad7cc6891ad0cc427a219672e7c78d37e50426
hobinLee <viny5120@gmail.com>
2021-08-18 23:47:42 +0900

    feat: Product Section에서 받는 Props 타입 추가 + 테스트

--- client/src/Pages/Main/ProductSection/ProductSection.test.tsx
+++ client/src/Pages/Main/ProductSection/ProductSection.test.tsx
@@ -1,13 +1,14 @@
 import { render } from "@/utils/test-util";
-import ProductSection from "./index";
+import ProductSection, { ProductSectionProps } from "./index";
 
-const ProductSectionPros = {
+const productSectionPros: ProductSectionProps = {
   title: "product-section-test",
+  type: "test",
 };
 
 describe("<ProductSection />", () => {
   it("should render component in document", () => {
-    const { container } = render(<ProductSection {...ProductSectionPros} />);
+    const { container } = render(<ProductSection {...productSectionPros} />);
     expect(container).toBeInTheDocument();
   });
 });
