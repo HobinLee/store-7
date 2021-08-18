675bff11e85eacc17eb1dbc3ea1ec34246bdd09b
jjunyjjuny <jjuny546@gmail.com>
2021-08-19 01:51:06 +0900

    feat: test section

--- client/src/Pages/MyPage/Section/Section.test.tsx
+++ client/src/Pages/MyPage/Section/Section.test.tsx
@@ -2,18 +2,29 @@ import "jest-styled-components";
 import { render } from "@/utils/test-util";
 import { screen } from "@testing-library/react";
 import Section, { SectionProps } from "./index";
+import { expectText } from "@/utils/test-util";
 
 const SectionProps: SectionProps = {
   title: "test title",
   description: "test description",
-  children: <div>children</div>,
+  children: <div data-testid="test__children">children</div>,
   lineType: "long1",
 };
 
 describe("<Section />", () => {
-  it("should render component in document", () => {
+  let page;
+  beforeEach(() => {
     const { container } = render(<Section {...SectionProps} />);
+    page = container;
+  });
 
-    expect(container).toBeInTheDocument();
+  it("should render component in document", () => {
+    expect(page).toBeInTheDocument();
+    expect(screen.getByTestId("test__children")).toBeInTheDocument();
+  });
+
+  it("should render text in document", () => {
+    expectText(SectionProps.title);
+    expectText(SectionProps.description);
   });
 });
