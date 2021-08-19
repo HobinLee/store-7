b1bbcafae8cb438697f801f50a577c9f2901a0fc
jjunyjjuny <jjuny546@gmail.com>
2021-08-19 16:33:48 +0900

    feat: question entity update

---
+++ server/src/product/entity/question.ts
@@ -0,0 +1,51 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from "typeorm";
+
+@Entity("qna")
+export class Question {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: "user_id", type: "int" })
+  userID: number;
+
+  @Column({ name: "product_id", type: "int" })
+  productID: number;
+
+  @Column({ type: "char", length: 5 })
+  type: string;
+
+  @Column({ length: 32 })
+  title: string;
+
+  @Column({ length: 100 })
+  question: string;
+
+  @Column({ type: "char", length: 32, nullable: true })
+  image: string;
+
+  @Column({ length: 100, nullable: true })
+  answer: string;
+
+  @Column({ type: "tinyint", name: "is_secret" })
+  isSecret: number;
+
+  @CreateDateColumn({
+    type: "timestamp",
+    name: "created_at",
+    default: () => "CURRENT_TIMESTAMP(6)",
+  })
+  createdAt: Date;
+
+  @CreateDateColumn({
+    type: "timestamp",
+    name: "answered_at",
+    default: () => "CURRENT_TIMESTAMP(6)",
+    nullable: true,
+  })
+  answeredAt: Date;
+}
