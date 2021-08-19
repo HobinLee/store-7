1b62d4a8007e81ea1341f0233ca9011acf103acd
LeagueLugas <leaguelugas@gmail.com>
2021-08-19 23:44:24 +0900

    feat: cors setting

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,12 @@ const serverPort = properties.server.port;
 const nestApplication = async () => {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(serverPort);
 };
 
