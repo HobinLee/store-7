af3d7f768fe8f84d03f30c91e8b191e367557b55
leejeongmin <danmin20@khu.ac.kr>
2021-08-20 03:10:16 +0900

    feat: optionBos css 수정

--- client/src/Pages/Detail/OptionBox.tsx
+++ client/src/Pages/Detail/OptionBox.tsx
@@ -99,19 +99,22 @@ const Wrapper = styled.div`
     background: ${({ theme }) => theme.color.background};
     width: 100%;
     padding: 1.5rem 0;
+    border-radius: 1rem;
     &__right {
       ${({ theme }) => theme.flexCenter}
-      ${gap("2rem")}
       .num-input {
         ${({ theme }) => theme.flexCenter}
+        margin-right: 2rem;
+        background: #fff;
         div {
           ${({ theme }) => theme.flexCenter}
           flex-direction: column;
+          height: 2.5rem;
           button {
             ${({ theme }) => theme.flexCenter};
             cursor: pointer;
             width: 1.6rem;
-            height: 1.6rem;
+            /* height: 1.6rem; */
             border: none;
             padding: 0.4rem;
             background: ${({ theme }) => theme.color.primary2};
@@ -120,10 +123,12 @@ const Wrapper = styled.div`
         &__up {
           transform: rotate(-90deg);
           fill: white;
+          height: 1.1rem;
         }
         &__down {
           transform: rotate(90deg);
           fill: white;
+          height: 1.2rem;
         }
       }
     }
