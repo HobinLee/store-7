88b3d8071854de6bf72047079083d1728d084b68
jjunyjjuny <jjuny546@gmail.com>
2021-08-20 02:40:37 +0900

    feat: api - my service/module

--- server/src/user/application/my-service.ts
+++ server/src/user/application/my-service.ts
@@ -2,10 +2,18 @@ import { Carts } from "@/cart/domain/carts";
 import { CartResponse } from "@/cart/dto/cart-response";
 import { Injectable } from "@nestjs/common";
 import { MyCartsResponse } from "../dto/my-response";
+import { Questions } from "@/product/domain/questions";
+import { Reviews } from "@/product/domain/reviews";
+import { QuestionResponse } from "@/product/dto/question-response";
+import { MyReviewResponse } from "@/product/dto/review-my-response";
 
 @Injectable()
 export class MyService {
-  constructor(private readonly carts: Carts) {}
+  constructor(
+    private readonly carts: Carts,
+    private readonly reviews: Reviews,
+    private readonly questions: Questions
+  ) {}
 
   async findMyCarts(userId: number): Promise<MyCartsResponse> {
     const data = await this.carts.findCartsByUserId(userId);
@@ -23,5 +31,33 @@ export class MyService {
       totalPayment,
       items: data.map(CartResponse.of),
     };
+  }
+
+  async getMyInfo(userId) {
+    return "";
+  }
+
+  async getMyReviews(userId) {
+    userId = 12;
+    const reviews = await this.reviews.findReviewByUserId(userId);
+    return reviews.map(MyReviewResponse.of);
+  }
+
+  async getMyQeustions(userId) {
+    userId = 12;
+    const questions = await this.questions.findQuestionsByUserId(userId);
+    return questions.map(QuestionResponse.of);
+  }
+
+  async getMyCurrentOrders() {
+    return "";
+  }
+
+  async getMyOrders() {
+    return "";
+  }
+
+  async patchMyInfo() {
+    return "";
   }
 }
--- server/src/user/user-module.ts
+++ server/src/user/user-module.ts
@@ -13,12 +13,19 @@ import { DestinationModule } from "src/destination/destination-module";
 import { MyController } from "./presentation/my-controller";
 import { CartModule } from "@/cart/cart-module";
 import { MyService } from "./application/my-service";
+import { Reviews } from "@/product/domain/reviews";
+import { Questions } from "@/product/domain/questions";
+import { Review } from "@/product/entity/review";
+import { Question } from "@/product/entity/question";
+import { ProductService } from "@/product/application/product-service";
+import { Products } from "@/product/domain/products";
+import { Product } from "@/product/entity/product";
 
 const jwtConfig = properties.auth;
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, Review, Question, Product]),
     JwtModule.register({
       secret: jwtConfig.secret,
       signOptions: { expiresIn: jwtConfig.expiresIn },
@@ -27,6 +34,16 @@ const jwtConfig = properties.auth;
     CartModule,
   ],
   controllers: [AuthController, UserController, MyController],
-  providers: [AuthService, UserService, Users, PasswordEncoder, MyService],
+  providers: [
+    AuthService,
+    UserService,
+    Users,
+    PasswordEncoder,
+    MyService,
+    Products,
+    ProductService,
+    Reviews,
+    Questions,
+  ],
 })
 export class UserModule {}
