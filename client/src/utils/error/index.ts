35f10aee7725c722304366e1747af1ca3c31c7d5
hobinLee <viny5120@gmail.com>
2021-08-21 03:16:45 +0900

    feat: error handler 작성

---
+++ client/src/utils/error/index.ts
@@ -0,0 +1,35 @@
+import { alert } from "@/Components/Alert";
+import { moveTo } from "@/Router";
+
+export const enum HttpStatus {
+  OK = 200,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  PAYMENT_REQUIRED = 402,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  NOT_ACCEPTABLE = 406,
+  PROXY_AUTHENTICATION_REQUIRED = 407,
+}
+
+interface ErrorResponse extends Response {
+  data?: string;
+}
+
+interface HttpError extends Error {
+  status: number;
+  response: ErrorResponse;
+}
+
+export const handleHttpError = (error: HttpError) => {
+  error.response.data && alert(error.response.data);
+
+  switch (error.status) {
+    case HttpStatus.UNAUTHORIZED:
+      moveTo("/login");
+      break;
+    default:
+      break;
+  }
+  throw Error(error.response.data || error.message);
+};
