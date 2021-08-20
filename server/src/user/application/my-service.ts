735bd2c70f0116774abc732ed99bcfc0cd3b4c41
jjunyjjuny <jjuny546@gmail.com>
2021-08-20 12:59:04 +0900

    feat: my service

--- server/src/user/application/my-service.ts
+++ server/src/user/application/my-service.ts
@@ -1,18 +1,28 @@
 import { Carts } from "@/cart/domain/carts";
 import { CartResponse } from "@/cart/dto/cart-response";
 import { Injectable } from "@nestjs/common";
-import { MyCartsResponse } from "../dto/my-response";
+import {
+  MyCartsResponse,
+  MyBasicInfoResponse,
+  MyCurrentOredersResponse,
+  MyOredersResponse,
+} from "../dto/my-response";
 import { Questions } from "@/product/domain/questions";
 import { Reviews } from "@/product/domain/reviews";
 import { QuestionResponse } from "@/product/dto/question-response";
 import { MyReviewResponse } from "@/product/dto/review-my-response";
+import { Users } from "../domain/users";
+import { MyInfoEditRequest } from "../dto/my-reqeust";
+import { Orders } from "@/order/domain/orders";
 
 @Injectable()
 export class MyService {
   constructor(
     private readonly carts: Carts,
     private readonly reviews: Reviews,
-    private readonly questions: Questions
+    private readonly questions: Questions,
+    private readonly users: Users,
+    private readonly orders: Orders
   ) {}
 
   async findMyCarts(userId: number): Promise<MyCartsResponse> {
@@ -34,30 +44,36 @@ export class MyService {
   }
 
   async getMyInfo(userId) {
-    return "";
+    userId = 1;
+    const user = await this.users.findUserById(userId);
+    return MyBasicInfoResponse.of(user);
+  }
+
+  async editMyInfo(request: MyInfoEditRequest) {
+    await this.users.updateUserInfo(request);
   }
 
   async getMyReviews(userId) {
-    userId = 12;
+    userId = 1;
     const reviews = await this.reviews.findReviewByUserId(userId);
     return reviews.map(MyReviewResponse.of);
   }
 
-  async getMyQeustions(userId) {
-    userId = 12;
+  async getMyQeustions(userId: number) {
+    userId = 1;
     const questions = await this.questions.findQuestionsByUserId(userId);
     return questions.map(QuestionResponse.of);
   }
 
-  async getMyCurrentOrders() {
-    return "";
+  async getMyCurrentOrders(userId: number) {
+    userId = 1;
+    const orders = await this.orders.findCurrentOrdersByUserId(userId);
+    return orders.map(MyCurrentOredersResponse.of);
   }
 
-  async getMyOrders() {
-    return "";
-  }
-
-  async patchMyInfo() {
-    return "";
+  async getMyOrders(userId: number) {
+    userId = 1;
+    const orders = await this.orders.findOrdersByUserId(userId);
+    return orders.map(MyOredersResponse.of);
   }
 }
