3c8967dab85a5d7fdd214e6310ec74836711cdfc
jjunyjjuny <jjuny546@gmail.com>
2021-08-22 14:42:58 +0900

    chore: 이미지 래퍼 폴더화, 이름변경

---
+++ client/src/Components/ToggleImageWrapper/index.tsx
@@ -0,0 +1,28 @@
+import { Loading, Search } from "@/assets";
+import styled from "styled-components";
+
+const ToggleImageWrapper = ({ src }: { src: string }) => {
+  return (
+    <Wrapper>
+      <img src={src} />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  overflow: hidden;
+
+  ${({ theme }) => theme.borderRadius.medium}
+
+  & > img {
+    display: block;
+    width: 100%;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: scale(1.03);
+    }
+  }
+`;
+
+export default ToggleImageWrapper;
