09d3f0a62a5344e233b8dee6db287be13365e676
jjunyjjuny <jjuny546@gmail.com>
2021-08-22 14:46:30 +0900

    feat: reviews query

--- client/src/Pages/MyPage/ContentArea/contents/Review/index.tsx
+++ client/src/Pages/MyPage/ContentArea/contents/Review/index.tsx
@@ -2,19 +2,27 @@ import styled from "styled-components";
 import Section from "../../../Section";
 import Table from "../../../Table";
 import rows from "@/Pages/MyPage/Table/rows";
-import { reviews } from "@/shared/dummy";
+import { useMyReviews } from "@/api/my";
 
 const Review = () => {
+  const { status, data: reviews } = useMyReviews();
+
   return (
     <Wrapper data-testid="test__review-content">
       <Section title="상품문의" lineType="long1">
-        <Table ths={["번호", "제목", "날짜"]} ratio={[1, 7, 1]}>
-          {reviews.reviews.length === 0 ? (
-            <rows.Empty colSpan={3} message="게시글이 존재하지 않습니다." />
-          ) : (
-            reviews.reviews.map((re) => <rows.Review {...re} key={re.id} />)
-          )}
-        </Table>
+        {status !== "loading" ? (
+          <Table ths={["번호", "제목", "날짜"]} ratio={[1, 7, 1]}>
+            {reviews.length === 0 ? (
+              <rows.Empty colSpan={3} message="게시글이 존재하지 않습니다." />
+            ) : (
+              reviews.map((review) => (
+                <rows.Review {...review} key={review.id} />
+              ))
+            )}
+          </Table>
+        ) : (
+          <div>스켈레톤 UI</div>
+        )}
       </Section>
     </Wrapper>
   );
