698fe007d15c6b4609521158583172fbfdc7a08f
hobinLee <viny5120@gmail.com>
2021-08-24 18:30:14 +0900

    fix: 충돌해결

--- client/src/styles/global-style.ts
+++ client/src/styles/global-style.ts
@@ -73,4 +73,23 @@ export const GlobalStyle = createGlobalStyle`
       display: none;
     }
   }
+
+  @font-face {
+    font-family: 'BMEULJIRO';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.0/BMEULJIRO.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'BMKIRANGHAERANG';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMKIRANGHAERANG.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'BMDOHYEON';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMDOHYEON.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
 `;
