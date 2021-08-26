9f8ec6f3bf6ca4cefe512d6677ba3bb8c65b9e5f
leejeongmin <danmin20@khu.ac.kr>
2021-08-27 02:54:49 +0900

    feat: postOrder 수정

---
+++ client/src/Pages/Order/OrderContent/LoggedoutContent.tsx
@@ -0,0 +1,222 @@
+import { useState } from "react";
+import useInput from "@/hooks/useInput";
+import ItemInfoBox from "@/Components/ItemInfoBox";
+import { Arrow, ETPay, KakaoPay } from "@/assets";
+import InputSection from "@/Components/Input/InputSection";
+import useValidation from "@/hooks/useValidation";
+import ValidationInput from "@/Components/Input/ValidationInput";
+import {
+  validateEmail,
+  validatePhoneNumber,
+  VALIDATION_ERR_MSG,
+} from "@/utils/validations";
+import {
+  AddressType,
+  DestinationType,
+  ICart,
+  PartialCart,
+} from "@/shared/type";
+import { postPaymentReady } from "@/api/payment";
+import properties from "@/config/properties";
+import CartOrderBox from "@/Components/CartOrderBox";
+import Address from "@/Components/Address";
+import { convertToPhoneNumber } from "@/utils/util";
+import { postOrder } from "@/api/orders";
+import { moveTo } from "@/Router";
+import { Title, Content, Info, Payment } from "../index";
+
+const LoggedoutContent = () => {
+  // 상품목록
+  const orderItems: { items: PartialCart[] } = JSON.parse(
+    localStorage.getItem("orders")
+  );
+
+  // form
+  const email = useInput("");
+  const emailValidation = useValidation(validateEmail);
+  const addressee = useInput("");
+  const nameValidation = useValidation((name: string) => !!name.length);
+  const phone = useInput("", convertToPhoneNumber);
+  const phoneValidation = useValidation(validatePhoneNumber);
+
+  // 결제수단
+  type paymentType = "kakaopay" | "etpay" | null;
+  const [payment, setPayment] = useState<paymentType>(null);
+
+  // 배송지
+  const [destination, setDestination] = useState<AddressType>({
+    address: "",
+    postCode: "",
+    detailAddress: "",
+  });
+  const handleChangeAddress = (address: DestinationType) => {
+    setDestination(address);
+  };
+
+  // 요청사항
+  const [request, setRequest] = useState("");
+
+  // create order
+  const handlePostOrder = () => {
+    orderItems.items.forEach(async (item) => {
+      await postOrder({
+        data: {
+          productId: item.productId,
+          addressee: addressee.value,
+          productOptionId: item.productOptionId,
+          price: item.price,
+          amount: item.amount,
+          destination: `${destination.address} ${destination.detailAddress}`,
+          request: request,
+        },
+      });
+    });
+  };
+
+  // 카카오페이
+  const handleKakaoPay = async () => {
+    const res = await postPaymentReady({
+      cid: "TC0ONETIME",
+      item_name: "item",
+      quantity: "1",
+      total_amount: "11",
+      tax_free_amount: "11",
+      approval_url: `${properties.baseURL}/payment/approve`,
+      cancel_url: properties.baseURL,
+      fail_url: properties.baseURL,
+    });
+    if (res) {
+      try {
+        handlePostOrder();
+      } finally {
+        window.open(res.url);
+      }
+    }
+  };
+
+  // 결제 버튼 클릭
+  const handlePay = () => {
+    if (payment === "kakaopay") handleKakaoPay();
+    else {
+      try {
+        handlePostOrder();
+      } finally {
+        moveTo("/order/success");
+      }
+    }
+  };
+
+  const isOrderable =
+    emailValidation.isValid &&
+    nameValidation.isValid &&
+    phoneValidation.isValid &&
+    !!payment &&
+    !!destination.postCode;
+
+  return (
+    <div className="contents">
+      <Title>
+        <span className="other">장바구니</span> <Arrow /> 주문/결제
+      </Title>
+
+      <Content>
+        <Info>
+          <div className="label">주문상품</div>
+          <div className="items">
+            {orderItems.items.map((cart) => (
+              <ItemInfoBox key={cart.id} {...(cart as ICart)} />
+            ))}
+          </div>
+        </Info>
+
+        <Info>
+          <div className="label">주문자</div>
+          <div className="user-info">
+            <InputSection title="이름">
+              <ValidationInput
+                input={addressee}
+                validation={nameValidation}
+                placeholder="이름"
+                message={VALIDATION_ERR_MSG.INVALID_NAME}
+              />
+            </InputSection>
+            <InputSection title="이메일">
+              <ValidationInput
+                input={email}
+                validation={emailValidation}
+                placeholder="이메일을 입력해주세요"
+                message={VALIDATION_ERR_MSG.INVALID_EMAIL}
+              />
+            </InputSection>
+            <InputSection title="휴대폰 번호" brief="휴대폰 번호를 적어주세요">
+              <ValidationInput
+                input={phone}
+                validation={phoneValidation}
+                placeholder="010-0000-0000"
+                message={VALIDATION_ERR_MSG.INVALID_PHONE}
+              />
+            </InputSection>
+          </div>
+        </Info>
+
+        <Info>
+          <div className="label">배송지</div>
+
+          <div className="address-info">
+            <Address onChangeAddress={handleChangeAddress} />
+
+            <div style={{ marginTop: "3rem" }}>
+              <select
+                className="order-input"
+                onChange={(e) => setRequest(e.target.value)}
+                defaultValue={request}
+              >
+                <option value="배송시 요청사항을 선택해주세요.">
+                  배송시 요청사항을 선택해주세요.
+                </option>
+                <option value="부재시 문 앞에 놓아주세요.">
+                  부재시 문 앞에 놓아주세요.
+                </option>
+                <option value="배송전에 미리 연락주세요.">
+                  배송전에 미리 연락주세요.
+                </option>
+                <option value="부재시 경비실에 맡겨주세요.">
+                  부재시 경비실에 맡겨주세요.
+                </option>
+                <option value="부재시 전화주시거나 문자 남겨 주세요.">
+                  부재시 전화주시거나 문자 남겨 주세요.
+                </option>
+              </select>
+            </div>
+          </div>
+        </Info>
+
+        <Info>
+          <div className="label">결제수단</div>
+
+          <div className="payments">
+            <Payment
+              className="payments__item"
+              onClick={() => setPayment("kakaopay")}
+              isClicked={payment === "kakaopay"}
+            >
+              <img width={70} src={KakaoPay} />
+              <div>카카오페이</div>
+            </Payment>
+            <Payment
+              className="payments__item"
+              onClick={() => setPayment("etpay")}
+              isClicked={payment === "etpay"}
+            >
+              <img width={70} src={ETPay} />
+              <div>ET페이</div>
+            </Payment>
+          </div>
+        </Info>
+      </Content>
+      <CartOrderBox {...{ handlePay }} isButtonDisabled={!isOrderable} />
+    </div>
+  );
+};
+
+export default LoggedoutContent;
