b9185cd234aea2e921c473f2dd12e506aafa0177
jjunyjjuny <jjuny546@gmail.com>
2021-08-26 16:55:18 +0900

    feat: question modal 개선

--- client/src/Pages/Detail/Question/QuestionModal/QuestionModal.test.tsx
+++ client/src/Pages/Detail/Question/QuestionModal/QuestionModal.test.tsx
@@ -1,13 +1,21 @@
+import { useMyQuestions } from "@/api/my";
 import { render } from "@/utils/test-util";
 import { screen } from "@testing-library/react";
-import QuestionModal, { OPTIONS } from "./index";
+import QuestionModal, { TYPES } from "./index";
 
 describe("<QuestionModal />", () => {
-  // it("should render component in document", () => {
-  //   const { container } = render(<QuestionModal handleModalOpen={() => {}} />);
-  //   expect(container).toBeInTheDocument();
-  // OPTIONS.forEach((option) => {
-  //   expect(screen.queryByText(option)).toBeInTheDocument();
-  // });
-  // });
+  it("should render component in document", () => {
+    const { refetch } = useMyQuestions();
+    const { container } = render(
+      <QuestionModal
+        handleModalOpen={() => {}}
+        submitType="post"
+        refetch={refetch}
+      />
+    );
+    expect(container).toBeInTheDocument();
+    TYPES.forEach((option) => {
+      expect(screen.queryByText(option)).toBeInTheDocument();
+    });
+  });
 });
--- client/src/Pages/Detail/Question/QuestionModal/index.tsx
+++ client/src/Pages/Detail/Question/QuestionModal/index.tsx
@@ -6,58 +6,62 @@ import { useState } from "react";
 import { gap } from "@/styles/theme";
 import { postQuestion, patchQuestion } from "@/api/questions";
 import Checkbox from "@/Components/Checkbox";
+import { QuestionForm } from "@/Pages/MyPage/ContentArea/contents/Question";
+import APIButton from "@/Components/APIButton";
+import { QueryObserverResult } from "react-query";
 
-export const OPTIONS = ["상품", "배송", "반품", "교환", "환불", "기타"];
+export const TYPES = ["상품", "배송", "반품", "교환", "환불", "기타"];
 interface QuestionModal {
-  handleModalOpen: (boolean) => void;
+  handleModalOpen: Function;
   submitType: string;
-  qeustion?: {
-    id?: number;
-    option: string;
-    value: string;
-    isSecret: boolean;
-  };
+  qeustion?: QuestionForm;
+  refetch: () => Promise<QueryObserverResult<unknown>>;
 }
 const QuestionModal = ({
   handleModalOpen,
-  qeustion = { option: "상품", value: "", isSecret: false },
+  qeustion = { type: "상품", question: "", isSecret: false },
   submitType,
+  refetch,
 }: QuestionModal) => {
-  const reviewVal = useInput(qeustion.value);
+  const questionVal = useInput(qeustion.question);
   const pathname = location.pathname.split("detail/")[1];
 
   // 문의 유형
-  const [option, setOption] = useState(qeustion.option);
-  const handleSetOption = (val: string) => {
-    setOption(val);
+  const [type, setType] = useState(qeustion.type);
+  const handleSetType = (val: string) => {
+    setType(val);
   };
 
   // 비밀글
-  const [isSecret, setIsSecret] = useState(qeustion.isSecret);
+  const [isSecret, setIsSecret] = useState<boolean>(qeustion.isSecret);
 
   const handleSumbit = async (e) => {
-    submitType === "post"
-      ? await postQuestion({
-          productId: parseInt(pathname),
-          type: option,
-          question: reviewVal.value,
-          isSecret: isSecret,
-        })
-      : submitType === "patch"
-      ? await patchQuestion({
-          id: qeustion.id,
-          question: {
-            type: option,
-            question: reviewVal.value,
-            isSecret,
-          },
-        })
-      : "";
+    if (submitType === "post") {
+      await postQuestion({
+        productId: parseInt(pathname),
+        type: type,
+        question: questionVal.value,
+        isSecret: isSecret,
+      });
+      handleModalOpen(false, true);
+      refetch();
+    } else if (submitType === "patch") {
+      await patchQuestion({
+        id: qeustion.id,
+        question: {
+          type: type,
+          question: questionVal.value,
+          isSecret,
+        },
+      });
+      handleModalOpen(false, true);
+      refetch();
+    }
   };
 
   return (
     <ModalWrapper title="문의하기" closeModal={() => handleModalOpen(false)}>
-      <Wrapper onSubmit={handleSumbit}>
+      <Wrapper>
         <div className="modal__content">
           <Checkbox
             isChecked={isSecret}
@@ -67,15 +71,15 @@ const QuestionModal = ({
             className="secret-checkbox"
           />
           <div className="modal__content-label">문의 유형</div>
-          <div className="modal__question-option">
-            {OPTIONS.map((item, idx) => (
-              <OptionBtn
+          <div className="modal__question-type">
+            {TYPES.map((item, idx) => (
+              <TypeBtn
                 key={idx}
-                onClick={() => handleSetOption(item)}
-                primary={option === item}
+                onClick={() => handleSetType(item)}
+                primary={type === item}
               >
                 {item}
-              </OptionBtn>
+              </TypeBtn>
             ))}
           </div>
         </div>
@@ -84,14 +88,13 @@ const QuestionModal = ({
           <div className="modal__content-label">문의 내용</div>
           <QuestionInput
             placeholder="문의 내용을 입력하세요."
-            defaultValue={reviewVal.value}
-            onChange={reviewVal.onChange}
+            defaultValue={questionVal.value}
+            onChange={questionVal.onChange}
           />
         </div>
-
-        <SubmitBtn type="submit" primary>
+        <APIButton api={handleSumbit} primary className="submit-btn">
           완료
-        </SubmitBtn>
+        </APIButton>
       </Wrapper>
     </ModalWrapper>
   );
@@ -113,7 +116,7 @@ const Wrapper = styled.form`
         ${({ theme }) => theme.font.medium};
       }
     }
-    &__question-option {
+    &__question-type {
       display: grid;
       grid-template-columns: repeat(3, 1fr);
     }
@@ -123,9 +126,13 @@ const Wrapper = styled.form`
     top: -3rem;
     left: 1rem;
   }
+  .submit-btn {
+    width: 100%;
+    margin-top: 3rem;
+  }
 `;
 
-const OptionBtn = styled(Button)`
+const TypeBtn = styled(Button)`
   ${({ theme }) => theme.font.medium};
   border: none;
 `;
@@ -136,11 +143,6 @@ const QuestionInput = styled.textarea`
   padding: 1.5rem;
   ${({ theme }) => theme.font.medium};
   ${({ theme }) => theme.borderRadius.medium};
-`;
-
-const SubmitBtn = styled(Button)`
-  width: 100%;
-  margin-top: 3rem;
 `;
 
 export default QuestionModal;
