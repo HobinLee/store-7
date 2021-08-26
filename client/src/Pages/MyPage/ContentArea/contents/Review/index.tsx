f662377d5ed3651a876c1f575231905d47a5801a
jjunyjjuny <jjuny546@gmail.com>
2021-08-26 15:56:44 +0900

    feat: 마이페이지 리뷰 수정/삭제

--- client/src/Pages/MyPage/ContentArea/contents/Review/index.tsx
+++ client/src/Pages/MyPage/ContentArea/contents/Review/index.tsx
@@ -1,27 +1,58 @@
 import styled from "styled-components";
 import Section from "../../../Section";
-import Table from "../../../Table";
-import rows from "@/Pages/MyPage/Table/rows";
 import { useMyReviews } from "@/api/my";
+import ReviewBox from "@/Pages/Detail/Review/ReviewBox";
+import { gap } from "@/styles/theme";
+import { useState } from "react";
+import ReviewModal from "@/Pages/Detail/Review/ReviewModal";
+
+export interface ReviewForm {
+  id?: number;
+  rate: string;
+  content: string;
+  image?: string;
+}
 
 const Review = () => {
-  const { status, data: reviews } = useMyReviews();
+  const { status, data: reviews, refetch } = useMyReviews();
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  const [review, setReview] = useState<ReviewForm | undefined>(undefined);
+
+  const handleClickEditButton = (review: ReviewForm) => {
+    setReview(review);
+    handleModalOpen(true);
+  };
+
+  const handleModalOpen = (val: boolean, justClose = false) => {
+    if (justClose) {
+      setIsModalOpened(false);
+      return;
+    }
+    if (!val) {
+      const submit = window.confirm(
+        "작성하고 있던 내용이 유실됩니다. 정말 다른 페이지로 이동하시겠어요?"
+      );
+      if (submit) setIsModalOpened(val);
+    } else setIsModalOpened(val);
+  };
 
   return (
     <Wrapper data-testid="test__review-content">
-      <Section title="상품문의" lineType="long1">
-        {status !== "loading" ? (
-          <Table ths={["번호", "제목", "날짜"]} ratio={[1, 7, 1]}>
-            {reviews.length === 0 ? (
-              <rows.Empty colSpan={3} message="게시글이 존재하지 않습니다." />
-            ) : (
-              reviews.map((review) => (
-                <rows.Review {...review} key={review.id} />
-              ))
-            )}
-          </Table>
-        ) : (
-          <div>스켈레톤 UI</div>
+      <Section
+        title="나의 상품후기"
+        description="회원님의 상품후기 내역입니다."
+      >
+        <ReviewsWrapper>
+          {status !== "loading" ? (
+            reviews.map((review) => (
+              <ReviewBox {...{ review, refetch, handleClickEditButton }} />
+            ))
+          ) : (
+            <div>스켈레톤 UI</div>
+          )}
+        </ReviewsWrapper>
+        {isModalOpened && (
+          <ReviewModal submitType="patch" {...{ handleModalOpen, review }} />
         )}
       </Section>
     </Wrapper>
@@ -29,4 +60,8 @@ const Review = () => {
 };
 const Wrapper = styled.div``;
 
+const ReviewsWrapper = styled.div`
+  margin-top: 3rem;
+  ${gap("3rem", "column")}
+`;
 export default Review;
