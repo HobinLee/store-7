81f30042de238daf1fee2800aa49461be7b60b6f
leejeongmin <danmin20@khu.ac.kr>
2021-08-26 13:27:08 +0900

    refactor: assets 정리

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -2,7 +2,6 @@ export { default as Banner1 } from "./banner1.gif";
 export { default as Banner2 } from "./banner2.gif";
 export { default as Banner3 } from "./banner3.gif";
 export { default as Logo } from "./logo.png";
-export { default as MenuIcon } from "./menu.svg";
 export { default as Close } from "./close.svg";
 export { default as Back } from "./back.svg";
 export { default as Arrow } from "./arrow.svg";
@@ -14,19 +13,19 @@ export { default as TrayImg } from "./tray.png";
 export { default as Cart } from "./cart.svg";
 export { default as Search } from "./search.svg";
 export { default as Loading } from "./loading.svg";
-export { default as UserIcon } from "./user.svg";
 export { default as AddUser } from "./addUser.svg";
 export { default as KakaoPay } from "./kakaopay.png";
 export { default as ETPay } from "./etpay.png";
 export { default as HeaderLogo } from "./header_logo.png";
 export { default as DeleveryIcon } from "./delivery.svg";
 export { default as Info } from "./info.svg";
-export { default as QuestionIcon } from "./question.svg";
 export { default as ReviewIcon } from "./review.svg";
 export { default as King } from "./king.svg";
 export { default as ProductIcon } from "./product.svg";
 export { default as ReadyIcon } from "./ready.svg";
 export { default as GoodIcon } from "./good.svg";
+
+// caregory banner
 export { default as CategoryBanner0 } from "./banners/0.gif";
 export { default as CategoryBanner1 } from "./banners/100.gif";
 export { default as CategoryBanner2 } from "./banners/200.gif";
@@ -37,6 +36,8 @@ export { default as CategoryBanner6 } from "./banners/600.gif";
 export { default as CategoryBanner7 } from "./banners/700.gif";
 export { default as CategoryBanner8 } from "./banners/800.gif";
 export { default as CategoryBanner9 } from "./banners/900.gif";
+
+// mypage
 export { default as DeliveryActive } from "./mypage/delivery_active.gif";
 export { default as WishActive } from "./mypage/wish_active.gif";
 export { default as QuestionActive } from "./mypage/question_active.gif";
