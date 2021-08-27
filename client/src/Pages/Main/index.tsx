f876b15ac4713fe9d2b297d96a30ae068b8e7953
jjunyjjuny <jjuny546@gmail.com>
2021-08-27 13:18:11 +0900

    feat: 메인페이지 링크 변경

--- client/src/Pages/Main/index.tsx
+++ client/src/Pages/Main/index.tsx
@@ -26,8 +26,8 @@ const MainPage = () => {
     <>
       <Header />
       <Wrapper>
-        <BannerSection />
         <Contents>
+          <BannerSection />
           <div className="main__contents-wrapper">
             <ProductSection {...SECTION_TYPES.hot} />
             <GiftSection />
