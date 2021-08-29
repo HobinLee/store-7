6fd12104c4123e026d17bbad55c95fa090faea6d
leejeongmin <danmin20@khu.ac.kr>
2021-08-29 13:42:23 +0900

    refactor: cart service error handling

--- server/src/config/messages.ts
+++ server/src/config/messages.ts
@@ -1,10 +1,18 @@
 export default {
   success: {
+    // user
     SUCCESS_TO_SIGN_IN: "로그인 성공",
     SUCCESS_TO_SIGN_OUT: "로그아웃 완료",
     SUCCESS_TO_SIGN_UP: "회원가입 완료",
+
+    // cart
+    SUCCESS_TO_FIND_CARTS_BY_USER_ID: "장바구니 가져오기 성공",
+    SUCCESS_TO_CREATE_CART: "장바구니 생성 성공",
+    SUCCESS_TO_UPDATE_CART: "장바구니 수정 성공",
+    SUCCESS_TO_DELETE_CART: "장바구니 삭제 성공",
   },
   failed: {
+    // user
     FAILED_TO_SIGN_IN: "이메일 또는 비밀번호가 다릅니다",
     FAILED_TO_GEN_JWT: "json web token 생성 실패",
     FAILED_TO_SIGN_UP: "계정생성에 실패했습니다.",
@@ -13,5 +21,11 @@ export default {
     EMAIL_EXIST: "이미 존재하는 이메일입니다",
     FAIL_GET_ACCESS_TOKEN: "엑세스 토큰을 얻어오는 데 실패했습니다",
     FAILED_TO_INSERT_ES: "검색 DB에 저장 실패",
+
+    //cart
+    FAILED_TO_FIND_CARTS_BY_USER_ID: "장바구니 가져오기 실패",
+    FAILED_TO_CREATE_CART: "장바구니 생성 실패",
+    FAILED_TO_UPDATE_CART: "장바구니 수정 실패",
+    FAILED_TO_DELETE_CART: "장바구니 삭제 실패",
   },
 };
