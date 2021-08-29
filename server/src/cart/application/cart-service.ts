a97e3faa7fff394f0d24ba3f5d72c25b1550ed33
jjunyjjuny <jjuny546@gmail.com>
2021-08-29 21:47:22 +0900

    chore: 에러핸들링

--- server/src/cart/application/cart-service.ts
+++ server/src/cart/application/cart-service.ts
@@ -20,27 +20,27 @@ export class CartService {
   createCart(userId: number, cart: CartRequest): string {
     try {
       this.carts.createCart({ ...cart, user: { id: userId } });
+      return messages.success.SUCCESS_TO_CREATE_CART;
     } catch (e) {
       throw new Error(messages.failed.FAILED_TO_CREATE_CART);
     }
-    return messages.success.SUCCESS_TO_CREATE_CART;
   }
 
   updateCart(id: number, modifiedCart: CartModifyRequest): string {
     try {
       this.carts.updateCart(id, modifiedCart);
+      return messages.success.SUCCESS_TO_UPDATE_CART;
     } catch (e) {
       throw new Error(messages.failed.FAILED_TO_UPDATE_CART);
     }
-    return messages.success.SUCCESS_TO_UPDATE_CART;
   }
 
   deleteCart(id: number): string {
     try {
       this.carts.deleteCart(id);
+      return messages.success.SUCCESS_TO_DELETE_CART;
     } catch (e) {
       throw new Error(messages.failed.FAILED_TO_DELETE_CART);
     }
-    return messages.success.SUCCESS_TO_DELETE_CART;
   }
 }
